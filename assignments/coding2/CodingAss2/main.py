1fb05f4cb1dd66514784b83a8f0bfdb6b1b0e235
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-11-30 14:32:36 -0700

    coding 2 start

---
+++ assignments/coding2/CodingAss2/main.py
@@ -0,0 +1,82 @@
+from utils import plot_data, generate_data
+import numpy as np
+"""
+Documentation:
+
+Function generate() takes as input "A" or "B", it returns X, t.
+X is two dimensional vectors, t is the list of labels (0 or 1).    
+
+Function plot_data(X, t, w=None, bias=None, is_logistic=False, figure_name=None)
+takes as input paris of (X, t) , parameter w, and bias. 
+If you are plotting the decision boundary for a logistic classifier, set "is_logistic" as True
+"figure_name" specifies the name of the saved diagram.
+"""
+
+def train_logistic_regression(X, t):
+    """
+    Given data, train your logistic classifier.
+    Return weight and bias
+    """
+
+    return w, b
+
+def predict_logistic_regression(X, w, b):
+    """
+    Generate predictions by your logistic classifier.
+    """
+
+    return t
+
+def train_linear_regression(X, t):
+    """
+    Given data, train your linear regression classifier.
+    Return weight and bias
+    """
+
+    return w, b
+
+def predict_linear_regression(X, w, b):
+    """
+    Generate predictions by your logistic classifier.
+    """
+
+    return t
+
+def get_accuracy(t, t_hat):
+    """
+    Calculate accuracy,
+    """
+    return acc
+
+def main():
+    # Dataset A
+    # Linear regression classifier
+    X, t = generate_data("A")
+    w, b = train_linear_regression(X, t)
+    t_hat = predict_linear_regression(X, w, b)
+    print("Accuracy of linear regression on dataset A:", get_accuracy(t_hat, t))
+    plot_data(X, t, w, b, is_logistic=False, figure_name='dataset_A_linear.png')
+
+    # logistic regression classifier
+    X, t = generate_data("A")
+    w, b = train_logistic_regression(X, t)
+    t_hat = predict_logistic_regression(X, w, b)
+    print("Accuracy of linear regression on dataset A:", get_accuracy(t_hat, t))
+    plot_data(X, t, w, b, is_logistic=True, figure_name='dataset_A_logistic.png')
+
+    # Dataset B
+    # Linear regression classifier
+    X, t = generate_data("B")
+    w, b = train_linear_regression(X, t)
+    t_hat = predict_linear_regression(X, w, b)
+    print("Accuracy of linear regression on dataset B:", get_accuracy(t_hat, t))
+    plot_data(X, t, w, b, is_logistic=False, figure_name='dataset_B_linear.png')
+
+    # logistic regression classifier
+    X, t = generate_data("B")
+    w, b = train_logistic_regression(X, t)
+    t_hat = predict_logistic_regression(X, w, b)
+    print("Accuracy of linear regression on dataset B:", get_accuracy(t_hat, t))
+    plot_data(X, t, w, b, is_logistic=True, figure_name='dataset_B_logistic.png')
+
+main()
