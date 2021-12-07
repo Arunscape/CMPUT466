b1544f44635dede3228cf639f71ba0d8e8527771
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-12-06 23:33:02 -0700

    it's fucked

--- assignments/coding2/C2.py
+++ assignments/coding2/C2.py
@@ -53,63 +53,82 @@ def readMNISTdata():
 #    ret[np.arange(x.size), x] = 1
 #    return ret
 
-def one_hot(x):
-    m = x.shape[0]
-    x = x[:,0]
-    oh = scipy.sparse.csr_matrix((np.ones(m), (x, np.array(range(m)))))
-    return np.array(oh.todense()).T
+#def one_hot(x):
+#    m = x.shape[0]
+#    x = x[:,0]
+#    oh = scipy.sparse.csr_matrix((np.ones(m), (x, np.array(range(m)))))
+#    return np.array(oh.todense()).T
 
 
-def accuracy(x, w, t):
-    prob = softmax(x @ w)
-    pred = np.argmax(prob, axis=1)
-    t = t.flatten()
-    num_correct = np.sum(pred == t)
-    num_incorrect = np.sum(pred != t)
-    return num_correct / len(pred)
+def one_hot(y):
+    global N_class
+    y_hot = np.zeros((len(y), N_class))
+    y_hot[np.arange(len(y)), y] = 1
+
+    return y_hot
+
+
+
+def accuracy(y, y_hat):
+    return np.sum(y==y_hat)/len(y)
     
 
 def softmax(z):
+    #exp = np.exp(z-np.max(z))
+    #for i in range(len(z)):
+    #    exp[i] /= np.sum(exp[i])
+    #return exp
     z -= np.max(z)
     sm = np.exp(z).T / np.sum(np.exp(z), axis=1)
     return sm.T
 
-def loss_gradient(x, w, t):
-    global lam
-    m = x.shape[0]
-    loss = -1/m * np.sum(one_hot(t) * np.log(softmax(x @ w))) + lam/2 * np.sum(w * w)
-    gradient = -1/m * x.T @ (one_hot(t) - softmax(x @ w)) + lam * w
+#def loss_gradient(x, w, t):
+#    global lam
+#    m = x.shape[0]
+#    loss = -1/m * np.sum(one_hot(t) * np.log(softmax(x @ w))) + lam/2 * np.sum(w * w)
+#    gradient = -1/m * x.T @ (one_hot(t) - softmax(x @ w)) + lam * w
+#
+#    return loss, gradient
 
-    return loss, gradient
+def get_loss(y, y_hat):
+    return -np.mean(np.log(y_hat[np.arange(len(y)), y]))
     
 
 # https://gist.github.com/awjuliani/5ce098b4b76244b7a9e3#file-softmax-ipynb
-def predict(X, W, t = None):
+def predict(X, w, t = None, debug=False):
     # X_new: Nsample x (d+1)
     # W: (d+1) x K
 
     # TODO Your code here
-    y = X @ W
-    t_hat = np.argmax(y, axis=1)
+    y = X @ w
+    y_hat = softmax(y)
+
+    if debug:
+        print("y: ", y)
+        print("y_hat: ", y_hat)
+
+    t_hat = np.argmax(y_hat, axis=1)
 
     # # of training samples
-    m = X.shape[0]
 
-    loss, _ = loss_gradient(X, W, t)
+    loss = get_loss(t, y_hat)
+    acc = accuracy(t.flatten(), t_hat)
 
-    acc = accuracy(X, W, t)
+    if debug:
+        print("t: ", t.flatten())
+        print("t_hat: ", t_hat)
 
 
     return y, t_hat, loss, acc
 
-
+# https://towardsdatascience.com/softmax-regression-in-python-multi-class-classification-3cb560d90cb2
 def train(X_train, y_train, X_val, t_val):
-    global lam, MaxEpoch
-
-    N_train = X_train.shape[0]
-    N_val   = X_val.shape[0]
+    global MaxEpoch
    
-    w = np.zeros([X_train.shape[1], N_class])
+    m, n = X_train.shape
+
+    # add column for bias
+    w = np.zeros((n, N_class))
 
     losses_train = []
     accuracies = []
@@ -117,30 +136,33 @@ def train(X_train, y_train, X_val, t_val):
     w_best = None
     epoch_best = 0
     acc_best = 0
+    
+    N_train = X_train.shape[0] # number of samples
+    N_val   = X_val.shape[0]   # number of validation samples
 
     for epoch in range(MaxEpoch):
         loss_this_epoch = 0
-        for b in range(int(np.ceil(N_train/batch_size)) ):
-            X_batch = X_train[b*batch_size : (b+1)*batch_size]
-            y_batch = y_train[b*batch_size : (b+1)*batch_size]
+        for i in range(int(np.ceil(N_train/batch_size)) ):
+        
+            X_batch = X_train[i*batch_size : (i+1)*batch_size]
+            y_batch = y_train[i*batch_size : (i+1)*batch_size]
 
-            loss_batch, g = loss_gradient(X_batch, w, y_batch)
+            y, _, loss_batch, _ = predict(X_batch, w, y_batch)
             loss_this_epoch += loss_batch
 
-            # TODO: Your code here
-            # Mini-batch gradient descent
-            w -= alpha*g - alpha*decay*w
+            y_hat = softmax(y)
+            y_hot = one_hot(y_batch)
 
-        # TODO: Your code here
-        # monitor model behavior after each epoch
-        # 1. Compute the training loss by averaging loss_this_epoch
+            w -= alpha / m * X_batch.T @ (y_hat - y_hot)
+
         training_loss = loss_this_epoch/int(np.ceil(N_train/batch_size))
         losses_train.append(training_loss)
-        # 2. Perform validation on the validation test by the risk
-        acc = accuracy(X_val, w, t_val)
+        _, _, _, acc = predict(X_val, w, t_val, debug=True)
         accuracies.append(acc)
-        # 3. Keep track of the best validation epoch, risk, and the weights
-        #print(risks_val)
+
+        print("loss: ", training_loss)
+        print("accuracy: ", acc)
+
         if acc_best < acc:
             epoch_best = epoch
             acc_best = acc
@@ -193,6 +215,8 @@ lam = 0
 #np.seterr(divide='ignore', invalid='ignore', over='ignore')
 
 epoch_best, acc_best, W_best, losses_train, acc_train = train(X_train, t_train, X_val, t_val)
+
+print("w: ", W_best)
 
 _, _, _, acc_test = predict(X_test, W_best, t_test)
 
